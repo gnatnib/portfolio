954a13519f4576a8c37496ab1bb40de06f5709e3
Bintang Syafrian Rizal <bintang.syafrian@gmail.com>
2025-01-27 16:07:39 +0700

    style

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -37,7 +37,7 @@ const experiences = [
   },
 
   {
-    title: "Software Engineering Intern at PT. UG Mandiri",
+    title: "Software Engineer Intern at PT. UG Mandiri",
     location: "Jakarta, Indonesia",
     date: "2025",
     description:
